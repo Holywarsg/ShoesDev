825451717eb70398aac66f3f331df1eeabc9eff0
Maxim Neskagu <maximneskagu@MacBook-Air-Maxim.local>
2023-08-31 00:03:35 +0300

    everything is working

--- shoes/shoes/urls.py
+++ shoes/shoes/urls.py
@@ -1,15 +1,15 @@
-    from django.contrib import admin
-    from django.urls import path, include
-    from rest_framework.routers import DefaultRouter
-    from .views import ShoesViewSet
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import ShoesViewSet
 
-    # Создание экземпляра DefaultRouter
-    router = DefaultRouter()
-    # Регистрация вашего ViewSet с роутером
-    router.register(r'shoes', ShoesViewSet)
-
-    urlpatterns = [
+# Создание экземпляра DefaultRouter
+router = DefaultRouter()
+# Регистрация вашего ViewSet с роутером
+router.register(r'shoes', ShoesViewSet)
+urlpatterns = [
         path('admin/', admin.site.urls),
         # Добавление путей, зарегистрированных в роутере, в общий список путей
         path('api/v1/', include(router.urls)),
+
     ]
